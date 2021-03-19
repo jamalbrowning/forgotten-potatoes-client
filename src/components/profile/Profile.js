c53113ab523822b1e49760cd3e9bd0f6c0b24d7b
Jim Browning <jamalebrowning@gmail.com>
2021-03-18 21:32:18 -0500

    Updated provider for reviews. wip review page,single review, and CUD for reviews

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,71 +1,50 @@
 import React, { useContext, useEffect } from "react";
 import "./profile.css";
 import { ProfileContext } from "./ProfileProvider";
-import { ProfileReviewCards } from "./profileReviewCards";
-import { Table } from "react-bootstrap"
+import { Table, Button } from "react-bootstrap";
+import { FaStar } from "react-icons/fa";
+import { Link } from 'react-router-dom'
 
 export const Profile = (props) => {
   const { reviews, getReviewsByUserId } = useContext(ProfileContext);
 
-  const userId = localStorage.getItem("user_id");
-  console.log("user id", userId);
   useEffect(() => {
+    const userId = localStorage.getItem("user_id");
     getReviewsByUserId(userId);
   }, []);
-  console.log("review", reviews.results);
-  const reviewCards =
-    reviews.length > 0 ? (
-      reviews.map((review) => (
-          
-          <div>
-            <tr>
-              <td>{review.menu_item_id.menu.rest_id.name} </td>
-          <td>{review.rating}</td>
-          <td>{review.menu_item_id.name}</td>
-          </tr>
-            
 
-          </div>
-        
-      ))
-    ) : (
-      <h1>You haven't written any reviews.</h1>
-    );
   return (
-    <div className="home-container">
+    <div className="review-container">
       <div className="preview-container">
-      <Table striped border hover>
-      <thead>
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Username</th>
-    </tr>
-  </thead>
-            
-            <tbody>
-              {
-               reviews && reviews.length > 0 ? (
-                  reviews.map((review) => (
-                      
-                      
-                        <tr>
-                          <td>{review.menu_item_id.menu.rest_id.name} </td>
-                      <td>{review.rating}</td>
-                      <td>{review.menu_item_id.name}</td>
-                      </tr>
-                        
-            
-                     
-                    
-                  ))
-                ) : (
-                  <h1>You haven't written any reviews.</h1>
-                )
-              } 
-            </tbody>
-      </Table>
-          </div>
+        <h3 className="text-center">Recent Reviews</h3>
+        <Table striped border hover variant="dark">
+          <thead>
+            <tr>
+              <th>Restaurant</th>
+              <th>Menu Item</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {reviews && reviews.length > 0 ? (
+              reviews.map((review) => (
+                <tr>
+                  <td ><Link to={`/singlereview/${review.id}`}>{review.menu_item_id.menu.rest_id.name} </Link></td>
+                  <td>{review.menu_item_id.name}</td>
+                  <td>
+                    {review.rating}/5 <FaStar size="1rem" />{" "}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <h1>You haven't written any reviews.</h1>
+            )}
+          </tbody>
+        </Table>
+        <Button href="/new-review">New Review</Button>
+        <Button href="/reviews">hello</Button>
+      </div>
     </div>
   );
 };
---
+++ src/components/review/ReviewProvider.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+
+export const ReviewContext = React.createContext();
+
+export const ReviewProvider = (props) => {
+  const [reviews, setReviews] = useState({});
+  const [ review, setReview ] = useState({})
+
+  const getReviews = () => {
+    return fetch('http://localhost:8000/reviewss',{
+        headers:{
+        "Authorization": `Token ${localStorage.getItem("fp_token")}`
+        }
+    })
+        .then(response => response.json())
+        .then(setReviews)
+    }
+
+  const getReviewsByUserId = (id) => {
+    return fetch(`http://localhost:8000/reviews?user=${id}`, {
+      headers: {
+        Authorization: `Token ${localStorage.getItem("fp_token")}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(setReviews);
+  };
+
+  const createReview = review => {
+    return fetch('http://localhost:8000/reviews', {
+      method: 'POST',
+      headers:{
+        "Authorization": `Token ${localStorage.getItem("fp_token")}`,
+        'Content-Type': 'application/json',
+        "Accept":"application/json",
+      },
+      body: JSON.stringify(review)
+    })
+      .then(response => response.json())
+      .then(getReviews)
+  }
+
+  const updateReview = review => {
+    return fetch(`http://localhost:8000/reviews/${review.id}`, {
+      method: 'PUT',
+      headers:{
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(review)
+    })
+      .then(getReviews)
+  }
+  const deleteReview = id => {
+    return fetch(`http://localhost:8000/reviews/${id}`, {
+        method: 'DELETE',
+    })
+        .then(getReviews)
+}
+
+const getReviewById = (id) => {
+  return fetch(`http://localhost:8000/reviews/${id}`)
+      .then(res => res.json())
+}
+
+  console.log("more", reviews);
+  return (
+    <ReviewContext.Provider
+      value={{
+        review,
+        reviews,
+        getReviewsByUserId,
+        createReview,
+        updateReview,
+        deleteReview,
+        getReviewById
+      }}
+    >
+      {props.children}
+    </ReviewContext.Provider>
+  );
+};
